735f1309d2ecb4fc6fd534a6ad82b2c3ebab1c5a
ssisodiya3 <ssisodiya3@gatech.edu>
2024-09-05 21:18:04 -0400

    image rotated

---
+++ assets/js/rotate.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let angle = 0;
+    const image = document.getElementById('rotating-image');
+
+    function rotateImage() {
+        angle += 1; // Increase the rotation angle
+        image.style.transform = `rotate(${angle}deg)`;
+        requestAnimationFrame(rotateImage); // Continuously call rotateImage
+    }
+
+    rotateImage(); // Start rotating the image
+});
